1389cf948417fb9da5f10601574d060cb75debc9
Edgar Talledos <onimenotsuki@gmail.com>
2018-07-02 20:06:35 -0500

    Creando una lista en React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const users = [{
+  username: 'lorem uno',
+  id: 1,
+  email: 'email@example.com',
+}, {
+  username: 'lorem dos',
+  id: 2,
+  email: 'email@example.com',
+}, {
+  username: 'lorem tres',
+  id: 3,
+  email: 'email@example.com',
+}];
+
+const listItems = users.map((user) =>
+  <li key={user.id}>
+    <ul>
+      <li>Nombre de usuario: {user.username}</li>
+      <li>Correo: {user.email}</li>
+    </ul>
+  </li>
+);
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <ul>{listItems}</ul>
+  </div>
+);
 
 export default App;
