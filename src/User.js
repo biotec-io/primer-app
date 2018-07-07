c870098f3b20da1e5ed990dcbd3ad7fec8e84661
Edgar Talledos <onimenotsuki@gmail.com>
2018-07-06 19:54:34 -0500

    Añadidas notificationes con UIkit

--- src/User.js
+++ src/User.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { notification } from 'uikit';
 
 /* en este parte utilizamos destructuring */
 /* { name, email } ==> props */
@@ -12,6 +13,7 @@ class User extends Component {
 
     this.state = {
       text: 'No se ha ejecutado ninguna acción',
+      image: 'http://via.placeholder.com/200x200',
     };
   }
 
@@ -21,11 +23,20 @@ class User extends Component {
     /* this.setState({ text: text }); */
   }
 
+  triggerNotification(text) {
+    notification(text, { status: 'primary' });
+    this.setState({ image: 'http://via.placeholder.com/200x200?text=Hola+Eduardo' });
+  }
+
   render() {
     return (
       <div key={this.props.id}>
         <div className="uk-card uk-card-default uk-card-body">
           <h3 className="uk-card-title">Nombre: {this.props.name}</h3>
+          <img
+            onClick={() => this.triggerNotification(this.props.name)}
+            src={this.state.image}
+          />
           <p><strong>Correo electrónico:</strong> {this.props.email}</p>
 
           <button
